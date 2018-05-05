43b3053d2ce1c47f4e3c562c43bd986f53fa27c4
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-05-06 00:11:26 +0300

    Интегрировал либу, теперь надо создать файл

--- CursachParser/GlobalClass.h
+++ CursachParser/GlobalClass.h
@@ -1,9 +1,11 @@
 #include <regex>
+#include "tinyxml2.h"
 #pragma once
 
 namespace Globals
 {
 	using namespace System;
+	using namespace tinyxml2;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
 	using namespace System::Windows::Forms;
@@ -17,10 +19,30 @@ namespace Globals
 		
 		static String^ XMLFileGlobalPatch;
 
-		static String^ GetXML()
+		static void GetXML()
 		{
-			MessageBox::Show(XMLFileGlobalPatch);
-			return "OK";
+			XMLDocument doc;
+			doc.LoadFile("../dream.xml");
+
+			// Structure of the XML file:
+			// - Element "PLAY"      the root Element, which is the
+			//                       FirstChildElement of the Document
+			// - - Element "TITLE"   child of the root PLAY Element
+			// - - - Text            child of the TITLE Element
+
+			// Navigate to the title, using the convenience function,
+			// with a dangerous lack of error checking.
+			const char* title = doc.FirstChildElement("PLAY")->FirstChildElement("TITLE")->GetText();
+		
+			String^ clistr = gcnew String(title);
+			MessageBox::Show("Name of play (1):"+ clistr +"\n");
+
+			// Text is just another Node to TinyXML-2. The more
+			// general way to get to the XMLText:
+			XMLText* textNode = doc.FirstChildElement("PLAY")->FirstChildElement("TITLE")->FirstChild()->ToText();
+			title = textNode->Value();
+			String^ clistr1 = gcnew String(title);
+			MessageBox::Show("Name of play (2): "+ clistr1 +"\n");
 		}
 
 	};
